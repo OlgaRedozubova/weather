cc145a79e7586697b71742472a8472e66875740a
olga redozubova <olgared82@.gmail.com>
2018-04-03 00:00:13 +0400

    home

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-
 const express = require('express');
 const server = express();
 const fs = require('fs');
 //const weatherRouter = require('./routes/weather');
 const router = express.Router();
-
 const rp = require('request-promise');
-var cheerio = require('cheerio'); // Basically jQuery for node.js
+//var cheerio = require('cheerio'); // Basically jQuery for node.js
 const appid = "b6907d289e10d714a6e88b30761fae22";
+
+const port = process.env.PORT || 5000;
+
 
 
 /*
@@ -15,7 +16,7 @@ server.get('/', function (req, res) {
     res.send('Hello World!');
 });*/
 
-server.use(express.static(__dirname + "/public"));
+//server.use(express.static(__dirname + "/public"));
 
 
 router.route("/weather")
@@ -54,10 +55,9 @@ router.route("/weather")
             });
 
     });
+
 router.route("/weather/:id")
     .get( function (req, res) {
-        //console.log(res);
-    //res.send('id = ' + req.params.id);
         rp({
             uri: "http://samples.openweathermap.org/data/2.5/weather",
             qs: {
@@ -82,10 +82,8 @@ router.route("/weather/:id")
 server.use('/', router);
 
 
-
-server.listen(3000, function () {
-    console.log('Приклад застосунку, який прослуховує 3000-ий порт!');
-});
+server.listen(port, () => console.log(`Listening on port ${port}`));
+//server.listen(port, function () { console.log('Приклад застосунку, який прослуховує 3000-ий порт!');});
 
 
 /*
